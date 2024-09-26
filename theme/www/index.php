1dc2222d335a3ff3151e28d4984c2efa671e1aed
kaestel <martin@think.dk>
2024-09-26 09:21:10 +0200

    optimized conversion specs

--- theme/www/index.php
+++ theme/www/index.php
@@ -12,24 +12,75 @@ $FS = new FileSystem();
 $ImageClass = new Image();
 
 $files = $FS->files(PRIVATE_FILE_PATH."/convert");
+$output_path = PUBLIC_FILE_PATH."/converted/";
 
+function convert($in_file, $out_file, $options) {
+	global $ImageClass;
+	global $output_path;
+
+	$out_file .= ".".$options["format"];
+
+	$result = $ImageClass->convert($in_file, $output_path.$out_file, $options);
+	if($result) {
+		print "Converted: ".basename($in_file)." to ".$out_file."<br>";
+	}
+	else {
+		print "<span style=\"color: red;\">Failed</span>: ".basename($in_file)." to ".$out_file." (maybe you need to allow cropping)<br>";
+	}
+
+}
 
 foreach($files as $file) {
 
 	$extension = strrchr($file, "."); 
 	$file_name = substr(basename($file), 0, strrpos(basename($file), $extension));
 
-	$ImageClass->convert($file, PUBLIC_FILE_PATH."/converted/".$file_name.".avif", [
+	convert($file, $file_name, [
+		"width" => 600,
+		"height" => 400,
+		"allow_cropping" => true,
+		"compression" => 60,
+		"format" => "jpg"
+	]);
+	convert($file, $file_name."-small", [
+		"width" => 300,
+		"height" => 200,
+		"allow_cropping" => true,
+		"compression" => 60,
+		"format" => "jpg"
+	]);
+
+
+	convert($file, $file_name, [
+		"width" => 600,
+		"height" => 400,
+		"allow_cropping" => true,
 		"compression" => 60,
 		"format" => "avif"
 	]);
-	print "Converted: ".basename($file)." to ".$file_name.".avif<br>";
+	convert($file, $file_name."-small", [
+		"width" => 300,
+		"height" => 200,
+		"allow_cropping" => true,
+		"compression" => 60,
+		"format" => "avif"
+	]);
 
-	$ImageClass->convert($file, PUBLIC_FILE_PATH."/converted/".$file_name.".webp", [
+
+	convert($file, $file_name, [
+		"width" => 600,
+		"height" => 400,
+		"allow_cropping" => true,
 		"compression" => 75,
 		"format" => "webp"
 	]);
-	print "Converted: ".basename($file)." to ".$file_name.".webp<br>";
+	convert($file, $file_name."-small", [
+		"width" => 300,
+		"height" => 200,
+		"allow_cropping" => true,
+		"compression" => 75,
+		"format" => "webp"
+	]);
 
 }
 
